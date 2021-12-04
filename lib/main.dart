17892059b4ba088cd7d7e3138be46e44ff77ddb6
Jhohan David Noreña Páez <jnorena@bancolombia.com.co>
2021-12-04 15:08:32 -0500

    Login con facebook

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,12 +13,57 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Material App Bar'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        body: Stack(
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  _showFBButton(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Widget _showFBButton() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: ElevatedButton.icon(
+                onPressed: () => _loginFb(),
+                icon: FaIcon(
+                  FontAwesomeIcons.facebook,
+                  color: Colors.white,
+                ),
+                label: Text('Iniciar sesión con Facebook'),
+                style: ElevatedButton.styleFrom(
+                    primary: Color(0xFF3B5998), onPrimary: Colors.white)))
+      ],
+    );
+  }
+
+  void _loginFb() async {
+    await FacebookAuth.i.logOut();
+    var result = await FacebookAuth.i.login(
+      permissions: ["public_profile", "email"],
+    );
+
+    if (result.status == LoginStatus.success) {
+      final requestData = await FacebookAuth.i.getUserData(
+        fields:
+            "email, name, picture.width(800).heigth(800), first_name, last_name",
+      );
+      print(requestData);
+    }
   }
 }
