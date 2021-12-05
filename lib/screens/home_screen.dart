fcd369353ad9dd06c7c9b01946b3d668bebc0ae0
Jhohan David Noreña Páez <jnorena@bancolombia.com.co>
2021-12-04 22:49:26 -0500

    Consumo Metodo POST

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
 
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:parcial_final/components/loader_component.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:parcial_final/helpers/constans.dart';
 import 'package:parcial_final/models/response.dart';
 import 'package:parcial_final/models/token.dart';
+import 'package:parcial_final/screens/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:parcial_final/models/response.dart';
@@ -176,7 +180,7 @@ class _HomeScreenState extends State<HomeScreen> {
             return Color(0xFF120E43);
           }),
         ),
-        onPressed: () => print("Presionando boton de guardar.....!!!!!"),
+        onPressed: () => postForm(widget.token),
       ),
     );
   }
@@ -242,43 +246,117 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     );
   }
-}
 
-bool _validToken(Token token) {
-  if (DateTime.parse(token.expiration).isAfter(DateTime.now())) {
-    return true;
+  bool _validToken(Token token) {
+    if (DateTime.parse(token.expiration).isAfter(DateTime.now())) {
+      return true;
+    }
+
+    return false;
   }
 
-  return false;
-}
-
-Future<Null> _getForm(Token token) async {
-  Response response = await getForm(token);
-  print("Respuesta del metodo GET: $response");
-}
-
-Future<Response> getForm(Token token) async {
-  if (!_validToken(token)) {
-    return Response(
-        isSuccess: false,
-        message:
-            'Sus credenciales se han vencido, por favor cierre sesión y vuelva a ingresar al sistema.');
+  Future<Null> _getForm(Token token) async {
+    Response response = await getForm(token);
+    print("Respuesta del metodo GET: $response");
   }
 
-  var url = Uri.parse('${Constans.apiUrl}/api/Finals');
-  var response = await http.get(
-    url,
-    headers: {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-      'authorization': 'bearer ${token.token}',
-    },
-  );
-  var body = response.body;
-  print(body);
-  if (response.statusCode >= 400) {
-    return Response(isSuccess: false, message: body);
+  Future<Response> getForm(Token token) async {
+    if (!_validToken(token)) {
+      return Response(
+          isSuccess: false,
+          message:
+              'Sus credenciales se han vencido, por favor cierre sesión y vuelva a ingresar al sistema.');
+    }
+
+    var url = Uri.parse('${Constans.apiUrl}/api/Finals');
+    var response = await http.get(
+      url,
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'authorization': 'bearer ${token.token}',
+      },
+    );
+    var body = response.body;
+    print(body);
+    if (response.statusCode >= 400) {
+      return Response(isSuccess: false, message: body);
+    }
+    var decodedJson = jsonDecode(body);
+    return Response(isSuccess: true, result: decodedJson);
   }
-  var decodedJson = jsonDecode(body);
-  return Response(isSuccess: true, result: decodedJson);
+
+  bool _validateFields() {
+    bool isValid = true;
+
+    if (_email.isEmpty) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar tu email.';
+    } else if (!EmailValidator.validate(_email)) {
+      isValid = false;
+      _emailShowError = true;
+      _emailError = 'Debes ingresar un email válido.';
+    } else {
+      _emailShowError = false;
+    }
+    setState(() {});
+    return isValid;
+  }
+
+  void postForm(Token token) async {
+    if (!_validateFields()) {
+      return;
+    }
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+      await showAlertDialog(
+          context: context,
+          title: 'Error',
+          message: 'Verifica que estes conectado a internet.',
+          actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+          ]);
+      return;
+    }
+
+    Map<String, dynamic> request = {
+      'email': _email,
+      'qualification': _estrellas,
+      'theBest': _gusto,
+      'theWorst': _noGusto,
+      'remarks': _comentarios,
+    };
+
+    var url = Uri.parse('${Constans.apiUrl}/api/Finals');
+    var response = await http.post(
+      url,
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        'authorization': 'bearer ${token.token}'
+      },
+      body: jsonEncode(request),
+    );
+    print(request);
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (response.statusCode >= 400) {
+      print(
+          "error al consumir el metodo POST" + response.statusCode.toString());
+      return;
+    }
+
+    var body = response.body;
+    var decodedJson = jsonDecode(body);
+    print(decodedJson);
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
 }
